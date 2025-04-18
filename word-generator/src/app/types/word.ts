23c753418071ec96a4a14a2be84dce2a03568807
Mahamoud <mahamoud_hafed.mohamed@stud.htwk-leipzig.de>
2025-04-18 19:47:51 +0200

    small changes

--- word-generator/src/app/types/word.ts
+++ word-generator/src/app/types/word.ts
@@ -1,8 +1,8 @@
 export interface WordEntry {
-    lemma: string;
-    type: 'noun' | 'verb' | 'adjective';
-    level?: 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2';
-    translations?: Record<string, string[]>;
-    [key: string]: any; // to allow extra fields (like conjugation, etc.)
-  }
-  
+  lemma: string;
+  type: 'noun' | 'verb' | 'adjective';
+  level?: 'A1' | 'A2' | 'B1' | 'B2' | 'C1' | 'C2';
+  translations?: Record<string, string[]>;
+  conjugation?: string;
+  gender?: 'der' | 'die' | 'das';
+}
