0bfbc182bcec7532ba9f9452e21ca9605984aa67
Mahamoud <mahamoud_hafed.mohamed@stud.htwk-leipzig.de>
2025-04-19 00:30:39 +0200

    deploy plausiable

--- word-generator/src/app/page.tsx
+++ word-generator/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { Poppins } from 'next/font/google'
 import WordScreen from './components/wordScreen'
 
@@ -11,25 +10,13 @@ const poppins = Poppins({
 export default function Home() {
   return (
     <>
-      <Head>
-      <script defer data-domain="qamar.onrender.com" src="https://plausible.io/js/script.js"></script>
-        <title>German Word Generator</title>
-        
-        <meta
-          name="description"
-          content="Learn a new German word every time you hit Generate!"
-        />
-      </Head>
-
-      {/* ⚠️ Trial Version Notice */}
       <div className="bg-yellow-100 text-yellow-800 text-center text-sm px-4 py-2 font-medium shadow-md">
         هذا إصدار تجريبي، الرجاء عدم التقاط صور أو مشاركة الرابط
       </div>
 
-      {/* Main content */}
       <div className={`${poppins.variable} font-sans`}>
         <WordScreen />
       </div>
     </>
-  )
+  );
 }
