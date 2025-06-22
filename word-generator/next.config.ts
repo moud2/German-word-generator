eae677587e1e23ffc1cb40df8e0790a96710e8f5
Mahamoud <mahamoud_hafed.mohamed@stud.htwk-leipzig.de>
2025-06-22 16:33:02 +0200

    config file

--- word-generator/next.config.ts
+++ word-generator/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   eslint: {
-    ignoreDuringBuilds: true, 
+    ignoreDuringBuilds: true,
   },
 };
 
